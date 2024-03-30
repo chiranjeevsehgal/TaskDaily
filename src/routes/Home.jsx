11e6ede1ca29269ed2fd93cef3ced5483601e879
chiranjeevsehgal <chiranjeevsehgal@gmail.com>
2024-03-30 16:31:50 +0530

    Hero1 done

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,12 @@
+import HomeHero1 from "../components/HomeHero1";
 import Navbar from "../components/Navbar";
 
 function Home(){
     return (
+        <>
         <Navbar/>
+        <HomeHero1/>
+        </>
     )
 }
 
