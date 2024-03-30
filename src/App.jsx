21d2ae8bc0b71def3cf62abea4559d2364a5416a
chiranjeevsehgal <chiranjeevsehgal@gmail.com>
2024-03-30 17:21:49 +0530

    Minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './routes/Home';
 import Signup from "./components/auth/Signup";
+import Dashboard from "./routes/Dashboard";
+
 function App() {
   
   return (
@@ -10,6 +12,7 @@ function App() {
         <Route path="/">
           <Route path="" element={<Home />} />
           <Route path="signup" element={< Signup/>} />
+          <Route path="dashboard" element={< Dashboard/>} />
         </Route>
       </Routes>
     </BrowserRouter>
